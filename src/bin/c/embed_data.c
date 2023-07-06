e35c23f191b5c98885bb7c2f7887f29cdb8c1a2a
eutro <git@eutro.dev>
2023-07-06 02:26:58 +0100

    Add license headers

--- src/bin/c/embed_data.c
+++ src/bin/c/embed_data.c
@@ -1,7 +1,74 @@
-// modified from https://stackoverflow.com/questions/11813271/embed-resources-eg-shader-code-images-into-executable-library-with-cmake
+/**
+ * Copyright (C) 2023 eutro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
+
+int main(int argc, char **argv) {
+  if (argc < 4) {
+    fprintf(stderr, "Usage: %s <out-name> <in-name> <var-name>\n", argv[0]);
+    return 1;
+  }
+
+  FILE *in = fopen(argv[2], "r");
+  if (!in) {
+    perror(argv[2]);
+    return 1;
+  }
+  FILE *out = fopen(argv[1], "w");
+  if (!out) {
+    perror(argv[1]);
+    return 1;
+  }
+  char *varName = argv[3];
+
+  fseek(in, 0, SEEK_END);
+  size_t dataLen = ftell(in);
+  fseek(in, 0, SEEK_SET);
+
+  fprintf(out, "#include <stdint.h>\n");
+  fprintf(out, "#include <stdalign.h>\n");
+  fprintf(out, "static const struct {\n");
+  fprintf(out, "  alignas(uint32_t) unsigned char data[%zu];\n", dataLen);
+  fprintf(out, "} d = {{");
+
+  unsigned char buf[256];
+  unsigned countOnLine = 0;
+  while (!feof(in)) {
+    size_t read = fread(buf, 1, sizeof(buf), in);
+    if (ferror(in)) {
+      perror("error reading");
+    }
+    if (read == 0) break;
+    for (unsigned i = 0; i < read; ++i) {
+      if (countOnLine == 0) fprintf(out, "\n ");
+      fprintf(out, " 0x%02x,", buf[i]);
+      countOnLine = (countOnLine + 1) % 16;
+    }
+  }
+  fprintf(out, "\n}};\n");
+  fprintf(out, "const unsigned char *%s = d.data;\n", varName);
+  fprintf(out, "const uint32_t %s_len = %zu;\n\n", varName, dataLen);
+
+  fclose(in);
+  fclose(out);
+
+  return 0;
+}
 
 FILE* open_or_exit(const char *fname, const char *mode) {
   FILE *f = fopen(fname, mode);
@@ -10,52 +77,4 @@ FILE* open_or_exit(const char *fname, const char *mode) {
     exit(EXIT_FAILURE);
   }
   return f;
-}
-
-int main(int argc, char** argv) {
-  if (argc < 4) {
-    fprintf(stderr, "USAGE: %s {sym} {rsrc} {name}\n\n"
-        "  Creates `u8 {name}[]` in {sym}.c from the contents of {rsrc}\n",
-        argv[0]);
-    return EXIT_FAILURE;
-  }
-
-  const char *sym = argv[1];
-  FILE *in = open_or_exit(argv[2], "r");
-  const char *name = argv[3];
-
-  fseek(in, 0, SEEK_END);
-  size_t data_len = ftell(in);
-  fseek(in, 0, SEEK_SET);
-
-  char symfile[256];
-  snprintf(symfile, sizeof(symfile), "%s.c", sym);
-
-  FILE *out = open_or_exit(symfile, "w");
-  fprintf(out, "#include <stdint.h>\n");
-  fprintf(out, "#include <stdalign.h>\n");
-  fprintf(out, "static const struct {\n");
-  fprintf(out, "  alignas(uint32_t) unsigned char data[%zu];\n", data_len);
-  fprintf(out, "} d = {{\n");
-
-  unsigned char buf[256];
-  size_t nread = 0;
-  size_t linecount = 0;
-  do {
-    nread = fread(buf, 1, sizeof(buf), in);
-    size_t i;
-    for (i = 0; i < nread; i++) {
-      fprintf(out, "0x%02x, ", buf[i]);
-      if (++linecount == 16) { fprintf(out, "\n"); linecount = 0; }
-    }
-  } while (nread > 0);
-  if (linecount > 0) fprintf(out, "\n");
-  fprintf(out, "}};\n");
-  fprintf(out, "const unsigned char *%s = d.data;\n", name);
-  fprintf(out, "const uint32_t %s_len = %zu;\n\n", name, data_len);
-
-  fclose(in);
-  fclose(out);
-
-  return EXIT_SUCCESS;
 }
